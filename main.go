05a10f246f14141a4b9e84492306adec4a100ba4
gossmass <multimasspain@gmail.com>
2025-08-22 00:09:32 +0900

    Bunch of stuff

--- main.go
+++ main.go
@@ -2,96 +2,207 @@ package main
 
 import (
 	"fmt"
+	"image/color"
+	"math/rand/v2"
 	"sand/world"
 
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
 const (
-	TILE_SAND world.TileID = iota + 1
-	TILE_WATER
+	CELL_SAND world.CellID = iota + 1
+	CELL_WATER
+	CELL_OIL
+	CELL_SOLID
+	CELL_FIRE
+	CELL_ASH
 
-	TILE_MAX
+	CELL_MAX
 )
 
-func sandRule(w *world.World, x, y int) {
-	if w.Get(x, y+1) == world.TILE_VOID {
-		w.Swap(x, y, x, y+1)
-	} else if w.Get(x-1, y+1) == world.TILE_VOID {
-		w.Swap(x, y, x-1, y+1)
-	} else if w.Get(x+1, y+1) == world.TILE_VOID {
-		w.Swap(x, y, x+1, y+1)
+func randSideDir() int {
+	dir := rand.IntN(2)
+	if dir == 1 {
+		return 1
+	}
+
+	return -1
+
+}
+
+func sandRule(w *world.World, cell *world.Cell, x, y int) {
+	if w.IsVoid(x, y+1) {
+		w.Move(x, y, x, y+1)
+	} else if w.IsVoid(x-1, y+1) {
+		w.Move(x, y, x-1, y+1)
+	} else if w.IsVoid(x+1, y+1) {
+		w.Move(x, y, x+1, y+1)
 	}
 }
 
-func waterRule(w *world.World, x, y int) {
-	if w.Get(x, y+1) == world.TILE_VOID {
+func waterRule(w *world.World, cell *world.Cell, x, y int) {
+	down := w.GetID(x, y+1)
+
+	if down == world.CELL_VOID {
+		w.Move(x, y, x, y+1)
+	} else if down == CELL_SAND || down == CELL_OIL {
 		w.Swap(x, y, x, y+1)
 	} else {
-		if w.Get(x-1, y) == world.TILE_VOID {
-			w.Swap(x, y, x-1, y)
-		}
-		if w.Get(x+1, y) == world.TILE_VOID {
-			w.Swap(x, y, x+1, y)
+		dir := randSideDir()
+		if w.IsVoid(x+dir, y) {
+			w.Move(x, y, x+dir, y)
 		}
 	}
+}
+
+func oilRule(w *world.World, cell *world.Cell, x, y int) {
+
+}
+
+func fireRule(w *world.World, cell *world.Cell, x, y int) {
+
+}
+
+func ashRule(w *world.World, cell *world.Cell, x, y int) {
+
+}
+
+func getCellName(cellID world.CellID) string {
+	switch cellID {
+	case world.CELL_VOID:
+		return "VOID"
+	case CELL_SAND:
+		return "SAND"
+	case CELL_WATER:
+		return "WATER"
+	case CELL_OIL:
+		return "OIL"
+	case CELL_SOLID:
+		return "SOLID"
+	case CELL_FIRE:
+		return "FIRE"
+	case CELL_ASH:
+		return "ASH"
+	}
+
+	return "Undefined"
+}
+
+func getCellColor(cellID world.CellID) color.RGBA {
+	switch cellID {
+	case CELL_SAND:
+		return rl.Green
+	case CELL_WATER:
+		return rl.Blue
+	case CELL_SOLID:
+		return rl.Gray
+	case CELL_OIL:
+		return rl.NewColor(24, 24, 24, 255)
+	case CELL_FIRE:
+		return rl.NewColor(200, 10, 10, 255)
+	case CELL_ASH:
+		return rl.NewColor(190, 190, 190, 255)
+	}
+	return rl.Blank
+
 }
 
 func main() {
-	var boardSize int32 = 256
-	var boardScale float32 = 2.0
+	var boardWidth int32 = 128
+	var boardHeight int32 = 96
+	var boardScale float32 = 8.0
 	var cursorSize int = 3
-	var cursorTileID world.TileID = TILE_SAND
+	var cursorCellID world.CellID = CELL_SAND
+	var isPaused bool = false
 
-	rl.InitWindow(int32(float32(boardSize)*boardScale), int32(float32(boardSize)*boardScale), "Title")
+	rl.InitWindow(int32(float32(boardWidth)*boardScale), int32(float32(boardHeight)*boardScale), "Title")
 	defer rl.CloseWindow()
 
 	rl.SetTargetFPS(60)
 
-	w := world.New(int(boardSize), int(boardSize), boardScale)
-	w.AddRule(world.NewRule(sandRule, rl.Green), TILE_SAND)
-	w.AddRule(world.NewRule(waterRule, rl.Blue), TILE_WATER)
+	w := world.New(int(boardWidth), int(boardHeight), boardScale)
+	defer w.Destroy()
+
+	w.AddRule(world.NewRule(sandRule), CELL_SAND)
+	w.AddRule(world.NewRule(waterRule), CELL_WATER)
+	w.AddRule(world.NewRule(world.EmptyAction), CELL_SOLID)
+	w.AddRule(world.NewRule(oilRule), CELL_OIL)
+	w.AddRule(world.NewRule(fireRule), CELL_FIRE)
+	w.AddRule(world.NewRule(ashRule), CELL_ASH)
+
+	bgImg := rl.GenImageChecked(int(float32(boardWidth)*boardScale), int(float32(boardHeight)*boardScale), int(boardScale)*4, int(boardScale)*4, rl.DarkGray, rl.Black)
+	bgTex := rl.LoadTextureFromImage(bgImg)
+	rl.UnloadImage(bgImg)
+	defer rl.UnloadTexture(bgTex)
 
 	for !rl.WindowShouldClose() {
-		rl.BeginDrawing()
-		rl.ClearBackground(rl.RayWhite)
 
 		mx := int(rl.GetMouseX())
 		my := int(rl.GetMouseY())
 		lx, ly := w.ToLocalSpace(mx, my)
 
-		if rl.IsMouseButtonDown(rl.MouseButtonLeft) {
-			w.PutBlob(lx, ly, cursorSize, cursorTileID)
+		if rl.IsMouseButtonDown(rl.MouseButtonRight) {
+			cell := world.NewCell(getCellColor(cursorCellID), cursorCellID)
+			w.Put(lx, ly, cell)
+
+			if isPaused {
+				w.UpdateTexture()
+			}
 		}
 
-		if rl.IsMouseButtonPressed(rl.MouseButtonRight) {
-			if cursorTileID+1 == TILE_MAX {
-				cursorTileID = world.TILE_VOID
-			} else {
-				cursorTileID++
+		if rl.IsMouseButtonDown(rl.MouseButtonLeft) {
+			w.PutBlob(lx, ly, cursorSize, world.Cell{Color: getCellColor(cursorCellID), Type: cursorCellID})
+
+			if isPaused {
+				w.UpdateTexture()
 			}
 		}
 
 		if w := rl.GetMouseWheelMove(); w != 0 {
+			dir := 1
 			if w < 0 {
-				cursorSize = max(3, cursorSize-1)
+				dir = -1
+			}
+
+			if rl.IsKeyDown(rl.KeyLeftControl) {
+				cursorSize = min(max(3, cursorSize+dir*2), 51)
 			} else {
-				cursorSize += 2
+				next := cursorCellID + world.CellID(dir)
+
+				if next < world.CELL_VOID {
+					cursorCellID = CELL_MAX - 1
+				} else if next >= CELL_MAX {
+					cursorCellID = world.CELL_VOID
+				} else {
+					cursorCellID = next
+				}
 			}
 		}
 
-		w.Update()
+		if rl.IsKeyPressed(rl.KeyP) {
+			isPaused = !isPaused
+		}
+
+		if rl.IsKeyPressed(rl.KeyS) && isPaused {
+			w.Update()
+		}
+
+		if !isPaused {
+			w.Update()
+		}
+
+		rl.BeginDrawing()
+		rl.ClearBackground(rl.RayWhite)
+		rl.DrawTexture(bgTex, 0, 0, rl.White)
 		w.Render()
 
-		rl.DrawText(fmt.Sprintf("Size: %d\nTile: %d", cursorSize, cursorTileID), 10, 40, 20, rl.Black)
+		rl.DrawText(fmt.Sprintf("Pause: P (%v) | Step: S\nSize: %d\nTile: %s (%d)", isPaused, cursorSize, getCellName(cursorCellID), cursorCellID), 10, 40, 20, rl.RayWhite)
 
 		size := int32(float32(cursorSize*2) * boardScale)
 		x, y := w.ToGlobalSpace(lx-cursorSize, ly-cursorSize)
-		rl.DrawRectangleLines(int32(x), int32(y), size, size, rl.Red)
+		rl.DrawRectangleLines(int32(x)-1, int32(y)-1, size+2, size+2, rl.White)
 
 		rl.DrawFPS(10, 10)
 		rl.EndDrawing()
 	}
-
-	w.Destroy()
 }
--- world/world.go
+++ world/world.go
@@ -1,58 +1,76 @@
 package world
 
 import (
+	"cmp"
 	"image/color"
-	"log"
+	"math/rand/v2"
+	"slices"
 
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
-type Chunk struct {
-	buffer []rl.Color
+type CellID int
+
+const CELL_VOID CellID = 0
+
+type Move struct {
+	src  int
+	dst  int
+	swap bool
 }
 
-type TileID int
-
-type Action func(w *World, x, y int)
-
-type Rule struct {
-	action Action
-	color  color.RGBA
+type Cell struct {
+	Color color.RGBA
+	Type  CellID
 }
 
-const TILE_VOID TileID = 0
-
-func NewRule(action Action, color color.RGBA) *Rule {
-	return &Rule{
-		action: action,
-		color:  color,
+func NewCell(color color.RGBA, id CellID) *Cell {
+	return &Cell{
+		Color: color,
+		Type:  id,
 	}
 }
 
-func emptyRule(w *World, x, y int) {}
+type Action func(w *World, cell *Cell, x, y int)
+
+type Rule struct {
+	action Action
+}
+
+func NewRule(action Action) *Rule {
+	return &Rule{
+		action: action,
+	}
+}
+
+var EmptyAction Action = func(w *World, cell *Cell, x, y int) {}
 
 type World struct {
-	chunks      []Chunk
-	buffer      []color.RGBA
-	rules       map[TileID]*Rule
-	rulesColors map[color.RGBA]TileID
-	width       int
-	height      int
-	rt          rl.RenderTexture2D
-	scale       float32
+	buffer  []color.RGBA
+	cells   []*Cell
+	changes []Move
+	rules   map[CellID]*Rule
+	rt      rl.RenderTexture2D
+
+	width    int
+	height   int
+	scale    float32
+	position rl.Vector2
 }
 
 func New(width, height int, scale float32) *World {
 	w := &World{
-		width:  width,
-		height: height,
-		scale:  scale,
+		width:    width,
+		height:   height,
+		scale:    scale,
+		position: rl.Vector2{X: 0, Y: 0},
 	}
-	w.rules = make(map[TileID]*Rule)
-	w.rulesColors = make(map[color.RGBA]TileID)
 	w.buffer = make([]color.RGBA, width*height)
+	w.cells = make([]*Cell, width*height)
+	w.rules = make(map[CellID]*Rule)
 	w.rt = rl.LoadRenderTexture(int32(width), int32(height))
-	w.AddRule(NewRule(emptyRule, rl.Blank), TILE_VOID)
+
+	w.AddRule(NewRule(EmptyAction), CELL_VOID)
 
 	return w
 }
@@ -60,97 +78,160 @@ func New(width, height int, scale float32) *World {
 func (w *World) Destroy() {
 	rl.UnloadRenderTexture(w.rt)
 }
-
-func (w *World) AddRule(rule *Rule, tileID TileID) {
-	w.rules[tileID] = rule
-	w.rulesColors[rule.color] = tileID
-}
-
-func (w *World) to1D(x, y int) int {
-	return y*w.width + x
-}
-
-func (w *World) to1DCheck(x, y int) (int, bool) {
-	index := w.to1D(x, y)
-	return index, !(index < 0 || index > len(w.buffer)-1)
-}
-
-func (w *World) rawPut(index int, tileID TileID) {
-	if rule, ok := w.rules[tileID]; ok {
-		w.buffer[index] = rule.color
-	}
-
+func (w *World) AddRule(rule *Rule, id CellID) {
+	w.rules[id] = rule
 }
 
 func (w *World) ToGlobalSpace(x, y int) (int, int) {
-	return int(float32(x) * w.scale), int(float32(y) * w.scale)
+	return int(float32(x)*w.scale + w.position.X), int(float32(y)*w.scale + w.position.Y)
 }
 
 func (w *World) ToLocalSpace(x, y int) (int, int) {
-	return int(float32(x) / w.scale), int(float32(y) / w.scale)
+	return int(float32(x)/w.scale - w.position.X), int(float32(y)/w.scale - w.position.Y)
 }
 
-func (w *World) Put(x, y int, tileID TileID) {
-	if index, ok := w.to1DCheck(x, y); ok {
-		w.rawPut(index, tileID)
+func (w *World) InBounds(x, y int) bool {
+	return !(x < 0 || x > w.width-1 || y < 0 || y > w.height-1)
+}
+
+func (w *World) To1D(x, y int) int {
+	return y*w.width + x
+}
+
+func (w *World) To2D(index int) (int, int) {
+	return index % w.width, index / w.width
+}
+
+func (w *World) Put(x, y int, cell *Cell) {
+	if !w.InBounds(x, y) {
+		return
 	}
+
+	w.cells[w.To1D(x, y)] = cell
 }
 
-func (w *World) PutBlob(x, y, size int, tileID TileID) {
+func (w *World) PutBlob(x, y, size int, cellToCopy Cell) {
 	sx := max(0, x-size)
-	ex := min(w.width-1, x+size)
+	ex := min(w.width, x+size)
 	sy := max(0, y-size)
-	ey := min(w.height-1, y+size)
+	ey := min(w.height, y+size)
 
-	for x := sx; x < ex; x++ {
-		for y := sy; y < ey; y++ {
-			index := w.to1D(x, y)
-			w.rawPut(index, tileID)
+	for ix := sx; ix < ex; ix++ {
+		for iy := sy; iy < ey; iy++ {
+			w.cells[w.To1D(ix, iy)] = NewCell(cellToCopy.Color, cellToCopy.Type)
 		}
 	}
 }
 
-func (w *World) Get(x, y int) TileID {
-	if index, ok := w.to1DCheck(x, y); ok {
-		if ruleID, ok := w.rulesColors[w.buffer[index]]; ok {
-			return ruleID
-		}
+func (w *World) Get(x, y int) *Cell {
+	if !w.InBounds(x, y) {
+		return nil
 	}
-	return 0
+	return w.cells[w.To1D(x, y)]
+}
+
+func (w *World) GetID(x, y int) CellID {
+	if !w.InBounds(x, y) {
+		return CELL_VOID
+	}
+	cell := w.cells[w.To1D(x, y)]
+
+	if cell == nil {
+		return CELL_VOID
+	}
+	return cell.Type
+
+}
+
+func (w *World) IsVoid(x, y int) bool {
+	cell := w.Get(x, y)
+	if cell == nil {
+		return false
+	}
+	return cell.Type == CELL_VOID
+}
+
+func (w *World) move(srcX, srcY, dstX, dstY int, swap bool) {
+	src := w.To1D(srcX, srcY)
+	dst := w.To1D(dstX, dstY)
+
+	if src < 0 || src > len(w.cells)-1 || dst < 0 || dst > len(w.cells)-1 {
+		return
+	}
+
+	w.changes = append(w.changes, Move{src: src, dst: dst, swap: swap})
+}
+
+func (w *World) Move(srcX, srcY, dstX, dstY int) {
+	w.move(srcX, srcY, dstX, dstY, false)
 }
 
 func (w *World) Swap(x1, y1, x2, y2 int) {
-	index1, ok := w.to1DCheck(x1, y1)
-	if !ok {
-		return
+	w.move(x1, y1, x2, y2, true)
+}
+
+func (w *World) ApplyMoves() {
+	w.changes = slices.DeleteFunc(w.changes, func(move Move) bool {
+		dst := w.cells[move.dst]
+		if dst == nil {
+			return false
+		}
+		return dst.Type != CELL_VOID
+	})
+
+	slices.SortFunc(w.changes, func(a, b Move) int {
+		return cmp.Compare(a.src, b.dst)
+	})
+
+	w.changes = append(w.changes, Move{src: -1, dst: -1})
+
+	iprev := 0
+	for i := 0; i < len(w.changes)-1; i++ {
+		if w.changes[i+1].dst != w.changes[i].dst {
+			rand := iprev + rand.IntN(i-iprev+1)
+			cmd := w.changes[rand]
+
+			if cmd.swap {
+				w.cells[cmd.dst], w.cells[cmd.src] = w.cells[cmd.src], w.cells[cmd.dst]
+			} else {
+				w.cells[cmd.dst] = w.cells[cmd.src]
+				w.cells[cmd.src] = nil //NewCell(rl.Blank, CELL_VOID)
+			}
+
+			iprev = i + 1
+		}
 	}
-	index2, ok := w.to1DCheck(x2, y2)
-	if !ok {
-		return
-	}
-	w.buffer[index1], w.buffer[index2] = w.buffer[index2], w.buffer[index1]
+
+	w.changes = w.changes[:0]
 }
 
 func (w *World) Update() {
-	for x := 0; x < w.width; x++ {
-		for y := w.height - 1; y >= 0; y-- {
-			index := w.to1D(x, y)
-			col := w.buffer[index]
-
-			if ruleID, ok := w.rulesColors[col]; ok {
-				w.rules[ruleID].action(w, x, y)
-			} else {
-				log.Printf("Failed to get rule for color: %v\n", col)
+	for y := 0; y < w.height; y++ {
+		for x := 0; x < w.width; x++ {
+			cell := w.cells[w.To1D(x, y)]
+			if cell == nil {
+				continue
+			}
+			if rule, ok := w.rules[cell.Type]; ok {
+				rule.action(w, cell, x, y)
 			}
 		}
 	}
 
+	w.ApplyMoves()
+	w.UpdateTexture()
+}
+
+func (w *World) UpdateTexture() {
+	for i := 0; i < len(w.cells); i++ {
+		cell := w.cells[i]
+		if cell == nil {
+			continue
+		}
+		w.buffer[i] = w.cells[i].Color
+	}
 	rl.UpdateTexture(w.rt.Texture, w.buffer)
 }
-
 func (w *World) Render() {
-	rl.DrawTextureEx(w.rt.Texture, rl.Vector2{}, 0, w.scale, rl.White)
+	rl.DrawTextureEx(w.rt.Texture, w.position, 0, w.scale, rl.White)
 }
-
-// func (w *World) Render() {
-// }
